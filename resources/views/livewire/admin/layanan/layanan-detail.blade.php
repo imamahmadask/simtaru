fb722b47220d1487dca8e65eeafe8cbf22b354b0
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2025-09-12 18:17:39 +0800

    edit table on layanan

--- resources/views/livewire/admin/layanan/layanan-detail.blade.php
+++ resources/views/livewire/admin/layanan/layanan-detail.blade.php
@@ -2,39 +2,39 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Layanan /</span> Detail Layanan</h4>
         <div class="row">
-            <div class="col-4">
+            <div class="col">
                 <div class="card mb-4">
                     <div class="card-header d-flex align-items-center justify-content-between">
                         <h5 class="mb-0 fw-semibold">Data Layanan</h5>
                     </div>
                     <div class="card-body">
-                        <table class="table">
-                            <tr>
-                                <td>Layanan</td>
-                                <td>:</td>
-                                <td>
-                                    <strong>{{ $layanan->nama }}</strong>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Kode</td>
-                                <td>:</td>
-                                <td>
-                                    <strong>{{ $layanan->kode }}</strong>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Keterangan</td>
-                                <td>:</td>
-                                <td>
-                                    <strong>{{ $layanan->keterangan }}</strong>
-                                </td>
-                            </tr>
-                        </table>
+                        <div class="table-responsive text-nowrap">
+                            <table class="table">
+                                <tr>
+                                    <td>Layanan</td>
+                                    <td>:</td>
+                                    <td>
+                                        <strong>{{ $layanan->nama }}</strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Kode</td>
+                                    <td>:</td>
+                                    <td>
+                                        <strong>{{ $layanan->kode }}</strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Keterangan</td>
+                                    <td>:</td>
+                                    <td>
+                                        <strong>{{ $layanan->keterangan }}</strong>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-8">
                 <div class="card mb-4">
                     <div class="card-header d-flex align-items-center justify-content-between">
                         <h5 class="mb-0 fw-semibold">Tahapan</h5>
@@ -44,82 +44,90 @@
                         </button>
                     </div>
                     <div class="card-body">
-                        <table class="table table-bordered">
-                            <thead>
-                                <th>No</th>
-                                <th>Nama tahapan</th>
-                                <th>Urutan</th>
-                                <th>Persyaratan Berkas</th>
-                                <th>Actions</th>
-                            </thead>
-                            <tbody>
-                                @php
-                                    $no = 1;
-                                @endphp
-                                @foreach ($layanan->tahapan as $tahapan)
-                                    <div wire:key="{{ $tahapan->id }}">
-                                        <tr>
-                                            <td>
-                                                {{ $no++ }}
-                                            </td>
-                                            <td>
-                                                {{ $tahapan->nama }}
-                                            </td>
-                                            <td>
-                                                {{ $tahapan->urutan }}
-                                            </td>
-                                            <td>
-                                                @if ($tahapan->persyaratanBerkas->count() > 0)
-                                                    @foreach ($tahapan->persyaratanBerkas as $persyaratan)
-                                                        <ul>
-                                                            <li>
-                                                                {{ $persyaratan->nama_berkas }} -
-                                                                {{ $persyaratan->wajib == 1 ? 'Wajib' : 'Tidak Wajib' }}
-                                                                <button type="button"
-                                                                    wire:click="$dispatch('persyaratan-berkas-edit', { id: {{ $persyaratan->id }} })"
-                                                                    class="btn btn-primary btn-xs"
-                                                                    data-bs-toggle="modal"
-                                                                    data-bs-target="#editPersyaratanBerkasModal">
-                                                                    <i class="bx bx-edit"></i>
-                                                                </button>
-                                                                @teleport('body')
-                                                                    <!-- Edit  Persyaratan Berkas Modal -->
-                                                                    @livewire('admin.layanan.persyaratan.persyaratan-berkas-edit')
-                                                                @endteleport
-                                                            </li>
-                                                        </ul>
-                                                    @endforeach
-                                                @endif
-                                            </td>
-                                            <td>
-                                                <button
-                                                    wire:click="$dispatch('tahapan-edit', { id: {{ $tahapan->id }} })"
-                                                    type="button" class="btn btn-primary btn-sm mr-1"
-                                                    data-bs-toggle="modal" data-bs-target="#editTahapanModal">
-                                                    Edit
-                                                </button>
-                                                @teleport('body')
-                                                    <!-- Edit  Tahapan Modal -->
-                                                    @livewire('admin.layanan.tahapan.tahapan-edit')
-                                                @endteleport
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Nama tahapan</th>
+                                    <th>Urutan</th>
+                                    <th>Persyaratan Berkas</th>
+                                    <th>Actions</th>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($layanan->tahapan as $tahapan)
+                                        <div wire:key="{{ $tahapan->id }}">
+                                            <tr>
+                                                <td>
+                                                    {{ $no++ }}
+                                                </td>
+                                                <td>
+                                                    {{ $tahapan->nama }}
+                                                </td>
+                                                <td>
+                                                    {{ $tahapan->urutan }}
+                                                </td>
+                                                <td>
+                                                    @if ($tahapan->persyaratanBerkas->count() > 0)
+                                                        @foreach ($tahapan->persyaratanBerkas as $persyaratan)
+                                                            <ul>
+                                                                <li>
+                                                                    {{ $persyaratan->nama_berkas }} -
+                                                                    {{ $persyaratan->wajib == 1 ? 'Wajib' : 'Tidak Wajib' }}
+                                                                    <button type="button"
+                                                                        wire:click="$dispatch('persyaratan-berkas-edit', { id: {{ $persyaratan->id }} })"
+                                                                        class="btn btn-primary btn-xs"
+                                                                        data-bs-toggle="modal"
+                                                                        data-bs-target="#editPersyaratanBerkasModal">
+                                                                        <i class="bx bx-edit"></i>
+                                                                    </button>
+                                                                    @teleport('body')
+                                                                        <!-- Edit  Persyaratan Berkas Modal -->
+                                                                        @livewire('admin.layanan.persyaratan.persyaratan-berkas-edit')
+                                                                    @endteleport
+                                                                </li>
+                                                            </ul>
+                                                        @endforeach
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex gap-2">
+                                                        <button
+                                                            wire:click="$dispatch('tahapan-edit', { id: {{ $tahapan->id }} })"
+                                                            type="button" class="btn btn-primary btn-xs"
+                                                            data-bs-toggle="modal" data-bs-target="#editTahapanModal">
+                                                            <i class="bx bx-edit"></i>
+                                                        </button>
 
-                                                <button type="button" class="btn btn-primary btn-sm"
-                                                    wire:click="$dispatch('persyaratan-add', { tahapan_id: {{ $tahapan->id }}, layanan_id: {{ $layanan->id }} })"
-                                                    data-bs-toggle="modal" data-bs-target="#addPersyaratanModal">
-                                                    Add Persyaratan
-                                                </button>
-                                                @teleport('body')
-                                                    <!-- Create Persyaratan Modal -->
-                                                    @livewire('admin.layanan.persyaratan.persyaratan-berkas-create')
-                                                @endteleport
-                                            </td>
-                                        </tr>
-                                    </div>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                                        <button type="button" class="btn btn-primary btn-xs"
+                                                            wire:click="$dispatch('persyaratan-add', { tahapan_id: {{ $tahapan->id }}, layanan_id: {{ $layanan->id }} })"
+                                                            data-bs-toggle="modal"
+                                                            data-bs-target="#addPersyaratanModal">
+                                                            <i class="bx bx-plus"></i> Syarat
+                                                        </button>
+                                                    </div>
+                                                    @teleport('body')
+                                                        <!-- Edit  Tahapan Modal -->
+                                                        @livewire('admin.layanan.tahapan.tahapan-edit')
+                                                    @endteleport
+                                                    @teleport('body')
+                                                        <!-- Create Persyaratan Modal -->
+                                                        @livewire('admin.layanan.persyaratan.persyaratan-berkas-create')
+                                                    @endteleport
+                                                </td>
+                                            </tr>
+                                        </div>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div class="col-8">
+
 
             </div>
         </div>
