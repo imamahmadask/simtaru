8af09b8d0ceaeb0dcfda0a23e533c4e61bb66659
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2025-09-18 10:09:49 +0800

    fix login logo and show password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,13 +17,68 @@
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                    autofocus autocomplete="username" placeholder="Input Email" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+
+                <div class="flex rounded-md shadow-sm">
+                    <!-- Input password -->
+                    <input id="password" type="password" name="password" required autocomplete="current-password"
+                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-l-md block w-full"
+                        placeholder="Input Password">
+
+                    <!-- Tombol show/hide -->
+                    <button type="button"
+                        class="px-3 border border-gray-300 border-l-0 rounded-r-md text-gray-500 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+                        onclick="togglePassword()">
+                        <!-- Eye open -->
+                        <svg id="eye-open" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5
+                         12 5c4.478 0 8.268 2.943 9.542
+                         7-1.274 4.057-5.064
+                         7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+
+                        <!-- Eye closed -->
+                        <svg id="eye-closed" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hidden" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477
+                         0-8.268-2.943-9.542-7a9.956
+                         9.956 0 012.249-3.592m2.101-1.933A9.956
+                         9.956 0 0112 5c4.478 0 8.268
+                         2.943 9.542 7a9.964 9.964
+                         0 01-4.043 5.197M15
+                         12a3 3 0 11-6 0 3 3
+                         0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
+                        </svg>
+                    </button>
+                </div>
             </div>
+
+            <script>
+                function togglePassword() {
+                    const input = document.getElementById('password');
+                    const eyeOpen = document.getElementById('eye-open');
+                    const eyeClosed = document.getElementById('eye-closed');
+
+                    if (input.type === 'password') {
+                        input.type = 'text';
+                        eyeOpen.classList.add('hidden');
+                        eyeClosed.classList.remove('hidden');
+                    } else {
+                        input.type = 'password';
+                        eyeOpen.classList.remove('hidden');
+                        eyeClosed.classList.add('hidden');
+                    }
+                }
+            </script>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
@@ -34,7 +89,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
