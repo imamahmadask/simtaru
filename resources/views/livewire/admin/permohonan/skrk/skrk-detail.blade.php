756383b25f58b62a8a96ad92ed6ab6392fd5b198
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2025-09-30 13:24:28 +0800

    init : upload dokumen final skrk

--- resources/views/livewire/admin/permohonan/skrk/skrk-detail.blade.php
+++ resources/views/livewire/admin/permohonan/skrk/skrk-detail.blade.php
@@ -285,6 +285,18 @@
                         </div>
 
                         <div class="tab-pane fade" id="navs-pills-top-berkas" role="tabpanel">
+                            <div class="mb-3">
+                                <div class="d-flex flex-wrap gap-3">
+                                    @can('manageDataEntry', $skrk->permohonan)
+                                        @if ($skrk->is_validate)
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                                data-bs-target="#UploadDokumenFixModal">
+                                                <i class="bx bx-cloud-upload"></i> Dokumen SKRK Fix
+                                            </button>
+                                        @endif
+                                    @endcan
+                                </div>
+                            </div>
                             <div class="row">
                                 <div class="col-xl">
                                     <div class="card mb-4">
@@ -408,6 +420,44 @@
                             Ya
                         </button>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div wire:ignore.self class="modal fade" id="UploadDokumenFixModal" data-bs-backdrop="static" tabindex="-1"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel1">
+                            Upload Dokumen SKRK Fix
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+
+                    <form wire:submit="uploadDokumenFix">
+                        <div class="modal-body">
+                            @foreach ($persyaratan_berkas as $item)
+                                <div class="row">
+                                    <div class="col mb-3">
+                                        <label for="file_.{{ $item->id }}" class="form-label">Upload
+                                            {{ $item->nama_berkas }}</label>
+                                        <input type="file" class="form-control"
+                                            wire:model="file_.{{ $item->kode }}" id="file_.{{ $item->id }}">
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="submit" class="btn btn-primary">
+                                Upload
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
