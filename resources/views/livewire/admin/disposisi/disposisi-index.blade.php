b94c3ffc70610a7003498f24c44d604f86489421
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2025-10-24 11:30:23 +0800

    fix disposisi index modal

--- resources/views/livewire/admin/disposisi/disposisi-index.blade.php
+++ resources/views/livewire/admin/disposisi/disposisi-index.blade.php
@@ -121,6 +121,30 @@
                                         </div>
                                     </td>
                                 </tr>
+                            </div>
+                            <!-- Modal -->
+                            <div class="modal fade" id="catatanModal{{ $data->id }}" tabindex="-1"
+                                aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="catatanModalLabel">Catatan
+                                            </h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ $data->catatan }}
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-outline-secondary"
+                                                data-bs-dismiss="modal">
+                                                Tutup
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endforeach
                     </tbody>
                 </table>
@@ -203,6 +227,30 @@
                                             @endif
                                         </td>
                                     </tr>
+                                </div>
+                                <!-- Modal -->
+                                <div class="modal fade" id="catatanModal{{ $data->id }}" tabindex="-1"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="catatanModalLabel">Catatan
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                {{ $data->catatan }}
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-outline-secondary"
+                                                    data-bs-dismiss="modal">
+                                                    Tutup
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </tbody>
                     </table>
@@ -211,28 +259,6 @@
             <!--/ Basic Bootstrap Table -->
         @endif
     </div>
-    @if ($data)
-        <!-- Modal -->
-        <div class="modal fade" id="catatanModal{{ $data->id }}" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="catatanModalLabel">Catatan
-                        </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        {{ $data->catatan }}
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                            Tutup
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endif
     @teleport('body')
         <!-- Edit  Regustrasi Modal -->
         @livewire('admin.disposisi.disposisi-edit')
