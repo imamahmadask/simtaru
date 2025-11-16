ede88be7eff1eb4ce4df11c09fe8e9265d911375
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2025-11-16 20:50:45 +0800

    add trustproxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'cekRole' => CekRole::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
